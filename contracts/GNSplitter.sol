87ca8ffeea222c2e1cc426edd580d60d8d2d3ead
Lance Whatley <whatl3y@gmail.com>
2022-06-17 13:16:14 -0400

    require weight on new wallet addition

--- contracts/GNSplitter.sol
+++ contracts/GNSplitter.sol
@@ -18,6 +18,7 @@ contract GNSplitter is Ownable {
   }
 
   function addWallet(address _wallet, uint8 _weight) external onlyOwner {
+    require(_weight > 0, 'weight needs to be higher than 0');
     receivers.push(Receiver({ wallet: _wallet, weight: _weight }));
     overallWeight += _weight;
   }
